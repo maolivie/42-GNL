4134742b5f71abe0f13038038243379c55e3effa
Marc Olivieri <molivier@student.42lyon.fr>
2021-01-20 10:47:04 +0100

    Small commit on bonus.c to work at 42

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: molivier <molivier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/10 17:45:53 by molivier          #+#    #+#             */
-/*   Updated: 2021/01/12 18:44:55 by molivier         ###   ########lyon.fr   */
+/*   Updated: 2021/01/20 10:44:50 by molivier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,4 +104,32 @@ int		get_next_line(int fd, char **line)
 			return (handle_exceptions(-1, &lst, node, line));
 	}
 	return (handle_exceptions(ret, &lst, node, line));
+}
+
+int		get_next_line(int fd, char **line)
+{
+	char			buf[BUFFER_SIZE + 1];
+	static t_list	*lst = NULL;
+	t_list			*node;
+	char			*nl_ptr;
+	ssize_t			ret;
+
+	if ((node = get_node(&lst, fd)) == NULL)
+		return (-1);
+	if ((nl_ptr = ft_strchr(node->text, '\n')))
+		*nl_ptr = '\0';
+	if ((*line = ft_strdup(node->text)) == NULL)
+		return (handle_exceptions(-1, &lst, node, line));
+	while (nl_ptr == NULL)
+	{
+		ret = read(fd, buf, BUFFER_SIZE);
+		if (ret == 0 || ret == -1)
+			break ;
+		buf[ret] = '\0';
+		if ((nl_ptr = ft_strchr(buf, '\n')))
+			*nl_ptr = '\0';
+		if ((*line = ft_strappend(*line, buf)) == NULL)
+			return (handle_exceptions(-1, &lst, node, line));
+	}
+	return (handle_exceptions(ret, prout));
 }
