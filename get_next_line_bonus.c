7ae3ac9a0711582ab6dc63d7c0dc7a0072b5b6c3
Marc Olivieri <molivier@student.42lyon.fr>
2021-01-21 15:38:38 +0100

    GNL multi FD v1.0

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: molivier <molivier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/10 17:45:53 by molivier          #+#    #+#             */
-/*   Updated: 2021/01/21 13:33:47 by molivier         ###   ########lyon.fr   */
+/*   Updated: 2021/01/21 15:27:10 by molivier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,18 +68,29 @@ void	handle_exceptions(ssize_t ret, t_list **alst, t_list *node, char **line)
 	}
 }
 
-ssize_t	get_line(const char *src, char **line, t_list *node)
+ssize_t	save_text_read(t_list *node, char *nl)
 {
-	char	*nl_ptr;
+	char	*save;
 
-	nl_ptr = ft_strchr(src, '\n');
-	if (nl_ptr)
-		*nl_ptr = '\0';
+	save = ft_strdup(nl + 1);
+	if (save == NULL)
+		return (-1);
+	free(node->save);
+	node->save = save;
+	return (1);
+}
+
+ssize_t	get_line(char **line, const char *src, char **add_nl, t_list *node)
+{
+	*add_nl = ft_strchr(src, '\n');
+	if (*add_nl)
+		**add_nl = '\0';
 	if (src == node->save)
 	{
-		if (nl_ptr)
+		if (*add_nl)
 		{
-			if ((*line = ft_strdup(src)) == NULL)
+			*line = ft_strdup(src);
+			if (*line == NULL)
 				return (-1);
 		}
 		else
@@ -90,31 +101,11 @@ ssize_t	get_line(const char *src, char **line, t_list *node)
 	}
 	else
 	{
-		if ((*line = ft_strappend(*line, src)) == NULL)
+		*line = ft_strappend(*line, src);
+		if (*line == NULL)
 			return (-1);
 	}
-	return (nl_ptr ? 1 : 0);
-}
-
-ssize_t	save_text_read(t_list *node, const char *buf)
-{
-	char	*save;
-
-	if (node->save)
-	{
-		save = ft_strdup(node->save + ft_strlen(node->save) + 1);
-		if (save == NULL)
-			return (-1);
-		free(node->save);
-		node->save = save;
-	}
-	else
-	{
-		node->save = ft_strdup(buf + ft_strlen(buf) + 1);
-		if (node->save == NULL)
-			return (-1);
-	}
-	return (1);
+	return (*add_nl ? 1 : 0);
 }
 
 int		get_next_line(int fd, char **line)
@@ -122,6 +113,7 @@ int		get_next_line(int fd, char **line)
 	char			buf[BUFFER_SIZE + 1];
 	static t_list	*lst = NULL;
 	t_list			*node;
+	char			*nl;
 	ssize_t			ret;
 
 	if (fd < 0)
@@ -129,17 +121,17 @@ int		get_next_line(int fd, char **line)
 	node = get_node(&lst, fd);
 	if (node == NULL)
 		return (-1);
-	ret = get_line(node->save, line, node);
+	ret = get_line(line, node->save, &nl, node);
 	while (ret == 0)
 	{
 		ret = read(fd, buf, BUFFER_SIZE);
 		if (ret == 0 || ret == -1)
 			break ;
 		buf[ret] = '\0';
-		ret = get_line(buf, line, node);
+		ret = get_line(line, buf, &nl, node);
 	}
 	if (ret == 1)
-		ret = save_text_read(node, buf);
+		ret = save_text_read(node, nl);
 	handle_exceptions(ret, &lst, node, line);
 	return ((int)ret);
 }
